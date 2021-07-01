d663060364ffc5e0dbd914e390639c606442ab7b
Abhishek <ABHISHEKKUMAR998>
2021-07-01 10:41:37 +0530

    To Display logo  using property binding

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import React from 'react';
+import logo from './assets/bridgelogo.jpg';
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            title: `hello all `
+            title: 'hello all'
         }
     }
     render() {
         return ( 
         <div>
-        
            <h1> { this.state.title } </h1>
-       </div >
+           <img src={logo}
+           alt= 'bridgelabz logo' />
+       </div>
         );
     }
 }
