f5aca40c879a482c281219fa2b30bb2d72072918
Abhishek <ABHISHEKKUMAR998>
2021-07-01 12:26:52 +0530

    To display message from user input

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            title: 'hello all'
+           userName:''
         }
     }
     onClick=($event) =>{
         console.log("save button is clicked",$event);
         window.open(this.url,"_blank");
     }
+    onNameChange=(event) =>{
+        console.log("value is",event.target.value);
+       this.setState({userName:event.target.value})
+    }
     render() {
         return ( 
+        <>
         <div>
-           <h1> { this.state.title } </h1>
+           <h1> Hello { this.state.userName } </h1>
            <img src={logo} onClick={this.onClick}
            alt= 'bridgelabz logo' />
        </div>
+
+       <div>
+         <input onChange={this.onNameChange}/>
+       </div>
+       </>
         );
     }
 }
